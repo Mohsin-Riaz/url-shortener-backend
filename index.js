b4f2a20741ac44310a6dce1b4ea775e057c4fefd
Mohsin Riaz <riaz.mohsin@gmail.com>
2022-10-21 21:42:43 -0400

    removed headers

--- index.js
+++ index.js
@@ -11,34 +11,34 @@ const PORT = process.env.PORT || 5000
 connectDB()
 app.use(express.json())
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-    )
-    res.header(
-        'Access-Control-Allow-Headers',
-        'x-access-token, Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-})
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header(
+//         'Access-Control-Allow-Methods',
+//         'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+//     )
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'x-access-token, Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+// })
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
 app.use('/', require('./routes/root'))
 app.use('/v/', require('./routes/links-router'))
 
-app.all('*', (req, res) => {
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if (req.accepts('json')) {
-        res.json({ message: '404 Not Found' })
-    } else {
-        res.type('txt').send('404 Not Found')
-    }
-})
+// app.all('*', (req, res) => {
+//     res.status(404)
+//     if (req.accepts('html')) {
+//         res.sendFile(path.join(__dirname, 'views', '404.html'))
+//     } else if (req.accepts('json')) {
+//         res.json({ message: '404 Not Found' })
+//     } else {
+//         res.type('txt').send('404 Not Found')
+//     }
+// })
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')

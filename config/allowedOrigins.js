020b961a30333125fa2402023b56d5b0681ca7e8
Mohsin Riaz <riaz.mohsin@gmail.com>
2022-10-23 20:58:01 -0400

    origins set

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,4 @@
 const allowedOrigins = [
-    'https://shrtn.onrender.com',
-    'http://shrtn.onrender.com',
     'http://riaz-url-shortener.onrender.com',
     'https://riaz-url-shortener.onrender.com',
 ]
